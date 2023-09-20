d0e6ab2cdeabc46e0449eb5c8ecab8b0f022194d
Mechazzz <129221431+Mechazzz@users.noreply.github.com>
2023-09-20 13:35:23 +0200

    js file update 2023.09.20

---
+++ script.js
@@ -0,0 +1,31 @@
+function justShowTheResult (result) {
+ console.log (result);
+}
+
+function wordCreator (x, y) {
+    let finalWord = x + y;
+
+    return finalWord;
+}
+
+justShowTheResult (wordCreator("ház", "tető"));
+justShowTheResult (wordCreator("ugró", "deszka"));
+justShowTheResult (wordCreator("malac", "persely"));
+
+function numberSubstraction (x, y) {
+    let substractedValue = 0;
+    substractedValue = x - y;
+    
+    return substractedValue;
+}
+
+justShowTheResult (numberSubstraction(10, 5));
+
+function numberAddition (x, y) {
+    let addedValue = 0;
+    addedValue = x + y;
+
+    return addedValue;
+} 
+
+justShowTheResult (numberAddition(10, 5));
